40859613118b8e4b712646296dc5281c0cf1c594
kenee101 <elijah.usih@stu.cu.edu.ng>
2025-11-30 17:09:06 +0100

    Revert "adjusted error"
    This reverts commit 8d07582a49eaa2c193577d4f5a7585b24d687009.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 // import TestSanity from "./pages/TestSanity";
 import { Element } from "react-scroll";
 import { motion } from "motion/react";
